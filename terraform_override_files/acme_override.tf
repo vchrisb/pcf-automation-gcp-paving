527058447543443c743b250d0bf338e8e778a26c
vchrisb <vchrisb@users.noreply.github.com>
2019-02-06 16:02:06 +0100

    update

--- terraform_override_files/acme_override.tf
+++ terraform_override_files/acme_override.tf
@@ -24,11 +24,19 @@ resource "acme_registration" "reg" {
   email_address   = "${var.email}"
 }
 
+resource "null_resource" "dns" {
+  triggers {
+      sys_domain  = "${module.pas.sys_domain}"
+      apps_domain = "${module.pas.apps_domain}"
+      tcp_domain  = "${module.pas.tcp_domain}"
+    }
+} 
+
 resource "acme_certificate" "certificate" {
   account_key_pem           = "${acme_registration.reg.account_key_pem}"
   common_name               = "${var.env_name}.${var.dns_suffix}"
   subject_alternative_names = "${formatlist("%s.${var.env_name}.${var.dns_suffix}", local.subdomains)}"
-
+  depends_on                = ["google_dns_record_set.nameserver","null_resource.dns"]
   dns_challenge {
     provider                  = "gcloud"
     config {
@@ -41,8 +49,8 @@ resource "acme_certificate" "certificate" {
 output "ssl_cert" {
   sensitive = true
   value     = <<EOF
-"${acme_certificate.certificate.certificate_pem}"
-"${acme_certificate.certificate.issuer_pem}"
+${acme_certificate.certificate.certificate_pem}
+${acme_certificate.certificate.issuer_pem}
 EOF
 }
 
@@ -56,8 +64,8 @@ resource "google_compute_ssl_certificate" "certificate" {
   name_prefix = "${var.env_name}-pas-lbcert"
   description = "user provided ssl private key / ssl certificate pair"
   certificate = <<EOF
-"${acme_certificate.certificate.certificate_pem}"
-"${acme_certificate.certificate.issuer_pem}"
+${acme_certificate.certificate.certificate_pem}
+${acme_certificate.certificate.issuer_pem}
 EOF
   private_key = "${acme_certificate.certificate.private_key_pem}"
 
@@ -65,7 +73,6 @@ EOF
     create_before_destroy = true
   }
 }
-
 
 module "pas" {
   ssl_certificate   = "${google_compute_ssl_certificate.certificate.self_link}"
