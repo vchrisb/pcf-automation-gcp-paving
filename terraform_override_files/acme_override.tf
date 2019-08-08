424b7d230e74195b5058fa993c7941ff8a1c16ea
vchrisb <vchrisb@users.noreply.github.com>
2019-08-08 19:47:07 +0200

    reduce number of letsencrypt certs

--- terraform_override_files/acme_override.tf
+++ terraform_override_files/acme_override.tf
@@ -1,6 +1,5 @@
 locals {
-    subdomains    = ["*.apps", "*.sys", "*.login.sys", "*.uaa.sys"]
-    mesh_subdomains = ["*.mesh.apps"]
+    subdomains    = ["*.apps", "*.sys", "*.login.sys", "*.uaa.sys", "*.mesh.apps", "pcf"]
 }
 
 variable "email" {
@@ -23,7 +22,7 @@ resource "acme_registration" "reg" {
 
 resource "null_resource" "dns-propagation-wait" {
   provisioner "local-exec" {
-    command = "sleep 30"
+    command = "sleep 60"
   }
   triggers {
       sys_domain  = "${module.pas.sys_domain}"
@@ -36,7 +35,7 @@ resource "null_resource" "dns-propagation-wait" {
 
 ## CF certificate
 
-resource "acme_certificate" "pas-certificate" {
+resource "acme_certificate" "pcf-certificate" {
   account_key_pem           = "${acme_registration.reg.account_key_pem}"
   common_name               = "${var.env_name}.${var.dns_suffix}"
   subject_alternative_names = "${formatlist("%s.${var.env_name}.${var.dns_suffix}", local.subdomains)}"
@@ -56,14 +55,14 @@ resource "acme_certificate" "pas-certificate" {
 output "ssl_cert" {
   sensitive = true
   value     = <<EOF
-${acme_certificate.pas-certificate.certificate_pem}
-${acme_certificate.pas-certificate.issuer_pem}
+${acme_certificate.pcf-certificate.certificate_pem}
+${acme_certificate.pcf-certificate.issuer_pem}
 EOF
 }
 
 output "ssl_private_key" {
   sensitive = true
-  value     = "${acme_certificate.pas-certificate.private_key_pem}"
+  value     = "${acme_certificate.pcf-certificate.private_key_pem}"
 }
 
 resource "google_compute_ssl_certificate" "certificate" {
@@ -71,99 +70,19 @@ resource "google_compute_ssl_certificate" "certificate" {
   name_prefix = "${var.env_name}-pas-lbcert"
   description = "user provided ssl private key / ssl certificate pair"
   certificate = <<EOF
-${acme_certificate.pas-certificate.certificate_pem}
-${acme_certificate.pas-certificate.issuer_pem}
+${acme_certificate.pcf-certificate.certificate_pem}
+${acme_certificate.pcf-certificate.issuer_pem}
 EOF
-  private_key = "${acme_certificate.pas-certificate.private_key_pem}"
+  private_key = "${acme_certificate.pcf-certificate.private_key_pem}"
 
   lifecycle = {
     create_before_destroy = true
   }
-}
-
-
-## CF mesh certificate
-
-resource "acme_certificate" "pas-mesh-certificate" {
-  account_key_pem           = "${acme_registration.reg.account_key_pem}"
-  common_name               = "mesh.apps.${var.env_name}.${var.dns_suffix}"
-  subject_alternative_names = "${formatlist("%s.${var.env_name}.${var.dns_suffix}", local.mesh_subdomains)}"
-  depends_on                = ["google_dns_record_set.nameserver","null_resource.dns-propagation-wait"]
-  dns_challenge {
-    provider                  = "gcloud"
-    config {
-      GCE_PROJECT               = "${var.project}"
-      GCE_SERVICE_ACCOUNT       = "${var.service_account_key}"
-      GCE_PROPAGATION_TIMEOUT   = "600"
-      GCE_POLLING_INTERVAL      = "10"
-      GCE_TTL                   = "60"
-    }
-  }
-}
-
-output "mesh_ssl_cert" {
-  sensitive = true
-  value     = <<EOF
-${acme_certificate.pas-mesh-certificate.certificate_pem}
-${acme_certificate.pas-mesh-certificate.issuer_pem}
-EOF
-}
-
-output "mesh_ssl_private_key" {
-  sensitive = true
-  value     = "${acme_certificate.pas-mesh-certificate.private_key_pem}"
-}
-
-resource "google_compute_ssl_certificate" "mesh-certificate" {
-
-  name_prefix = "${var.env_name}-pas-mesh-lbcert"
-  description = "user provided ssl private key / ssl certificate pair"
-  certificate = <<EOF
-${acme_certificate.pas-mesh-certificate.certificate_pem}
-${acme_certificate.pas-mesh-certificate.issuer_pem}
-EOF
-  private_key = "${acme_certificate.pas-mesh-certificate.private_key_pem}"
-
-  lifecycle = {
-    create_before_destroy = true
-  }
-}
-
-
-### Opsman
-
-resource "acme_certificate" "opsman-certificate" {
-  account_key_pem           = "${acme_registration.reg.account_key_pem}"
-  common_name               = "${module.ops_manager.ops_manager_dns}"
-  depends_on                = ["google_dns_record_set.nameserver","null_resource.dns-propagation-wait"]
-  dns_challenge {
-    provider                  = "gcloud"
-    config {
-      GCE_PROJECT               = "${var.project}"
-      GCE_SERVICE_ACCOUNT       = "${var.service_account_key}"
-      GCE_PROPAGATION_TIMEOUT   = "600"
-      GCE_POLLING_INTERVAL      = "10"
-      GCE_TTL                   = "60"
-    }
-  }
-}
-
-output "opsman_ssl_cert" {
-  sensitive = true
-  value     = <<EOF
-${acme_certificate.opsman-certificate.certificate_pem}
-${acme_certificate.opsman-certificate.issuer_pem}
-EOF
-}
-
-output "opsman_ssl_private_key" {
-  sensitive = true
-  value     = "${acme_certificate.opsman-certificate.private_key_pem}"
 }
 
 ## update module
 
 module "pas" {
   ssl_certificate   = "${google_compute_ssl_certificate.certificate.self_link}"
-  mesh_ssl_certificate   = "${google_compute_ssl_certificate.mesh-certificate.self_link}"
+  mesh_ssl_certificate   = "${google_compute_ssl_certificate.certificate.self_link}"
 }
